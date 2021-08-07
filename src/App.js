a48a18570d6761aceef1911ea0ed761cbdbdef12
Bhushan <bhushan1810patil@gmail.com>
2021-08-08 00:19:49 +0530

    added a few more changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import "./App.css";
 import requests from "./requests.js";
 import Row from "./Row.js";
+import Banner from "./Banner.js";
+import Nav from "./Nav.js";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <Nav />
+      <Banner />
       <Row
         title="Trending"
         fetchURL={requests.fetchTrending}
         isLargeRow={true}
       />
-      <Row
+      {/* <Row
         title="Netflix Originals"
         fetchURL={requests.fetchNetflixOriginals}
-      />
+      /> */}
       <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
       <Row title="Action" fetchURL={requests.fetchActionMovies} />
       <Row title="Comedy" fetchURL={requests.fetchComedyMovies} />
